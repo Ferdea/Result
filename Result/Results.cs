4e25c129b1d0ef0622fab6b165119ea07993af9a
ferdea <sapegin04@mail.ru>
2025-03-12 21:28:06 +0500

    add extensions

--- Result/Results.cs
+++ Result/Results.cs
@@ -4,12 +4,20 @@ public static class Results
 {
     public static Result Error(Error error)
         => new(error);
+
+    public static Result Error<TErrorCode>(TErrorCode errorCode, string? message = null, params Error[] details)
+        where TErrorCode : IErrorCode
+        => new Error(errorCode, message, details);
     
     public static Result Ok()
         => new(null);
     
     public static Result<TValue> Error<TValue>(Error error)
         => new(error);
+    
+    public static Result<TValue> Error<TValue, TErrorCode>(TErrorCode errorCode, string? message = null, params Error[] details)
+        where TErrorCode : IErrorCode
+        => new Error(errorCode, message, details);
     
     public static Result<TValue> Ok<TValue>(TValue value)
         => new(value);
